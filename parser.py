f1631a54f8e55bf453747920e8776b0829dc4c06
tremolet <simontremolet@hotmail.com>
2016-04-13 12:44:47 +0200

    no message

--- parser.py
+++ parser.py
@@ -1,35 +1,32 @@
 import os
 import apache_log_parser
-#from pymongo import MongoClient
-#client = MongoClient()
-#db = client.logApache
-WORK_DIR = '~/Bureau'
+from pymongo import MongoClient
+client = MongoClient('localhost')
+db = client.logApache
+'''WORK_DIR = 'home/ubuntu/Bureau'
 file_path = os.path.join(WORK_DIR, 'access_log.txt')
 log_format = '%h %{cwp}C %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
 parser = apache_log_parser.make_parser(log_format)
 f=open('access_log.txt', 'r')
 content=f.readlines()
-for i in range(24,40):
+for i in range(0,len(content)):
 	log = parser(content[i])
         user_id = log['cookie_cwp']
 	timestamp = log['time_received_utc_datetimeobj']
 	server = log['request_header_referer']
 	rqst_verb = log['request_method']
-        rqst_url=log['request_url_path']
         if user_id is not '-':
          rqst_categ='login'
         else:
          rqst_categ='other'
-        if "GET /lib/" not in log['request_first_line']:
+        if log['request_first_line'].startswith("GET /lib/"):
           rqst_type='STATIC'
         else:
           rqst_type='API'
-        print user_id
-        print rqst_url
-        print rqst_verb
-        print rqst_categ
-        print rqst_type
-            
-    #db.log.insert_one({"user_id":user_id,'timestamp':timestamp,'server':server,'request':{'type':rqst_type,'verb':,'url':rqst_url,'categ':rqst_categ}})
+        rqst_url=log['request_url_path']
+        db.logApache.insert_one({"user_id":user_id,'timestamp':timestamp,'server':server,'request':{'type':rqst_type,'verb':rqst_verb,'url':rqst_url,'categ':rqst_categ}})'''
+doc=db.logApache.find_one({"server":'37.187.114.171'})
+print doc
+
 
 
